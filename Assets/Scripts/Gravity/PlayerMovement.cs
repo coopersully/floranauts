4814422bc3a923b5c3674592e332bbd3123472a4
Cooper Sullivan <thecoopersully@gmail.com>
2022-11-01 19:25:49 -0500

    Change input handler to input system

--- Assets/Scripts/Gravity/PlayerMovement.cs
+++ Assets/Scripts/Gravity/PlayerMovement.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
