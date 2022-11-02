100a70d403059474634f7e3c6852f5b91a191009
MacraeSmith <99697319+MacraeSmith@users.noreply.github.com>
2022-11-02 14:56:41 -0500

    Revert "Added Player Model"
    This reverts commit 3bdb855c27c1542beece16598becbae6b0b3112a.

--- Assets/Scripts/Gravity/PlayerMovement.cs
+++ Assets/Scripts/Gravity/PlayerMovement.cs
@@ -1,10 +1,8 @@
 using Player;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 namespace Gravity
 {
-<<<<<<< Updated upstream
     public class PlayerMovement : MonoBehaviour
     {
         InputManager inputManager;
@@ -29,37 +27,6 @@ namespace Gravity
         {
             inputManager = GetComponent<InputManager>();
             gravityControl = GetComponent<GravityControl>();
-=======
-    InputManager inputManager;
-    GravityControl gravityControl;
-    GravityAttractor gravityAttractor;
-    Animator animator;
-
-
-
-
-    public float mouseSensitivityX = 1;
-    public float mouseSensitivityY = 1;
-    public float walkSpeed = 10;
-    public float jumpForce = 500;
-    public LayerMask groundMask;
-    public Transform groundCheck;
-
-    public bool isGrounded;
-    Vector3 moveAmount;
-    Vector3 smoothMoveVelocity;
-    float verticalLookRotation;
-    Transform cameraTransform;
-    Rigidbody rb;
-
-    private void Awake()
-    {
-        inputManager = GetComponent<InputManager>();
-        gravityControl = GetComponent<GravityControl>();
-        animator = GetComponent<Animator>();
-
-
->>>>>>> Stashed changes
 
 
 
@@ -73,13 +40,7 @@ namespace Gravity
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
 
-<<<<<<< Updated upstream
         }
-=======
-        //groundCheck
-        isGrounded = Physics.CheckSphere(groundCheck.position, .5f, groundMask);
-        if (isGrounded) this.animator.SetBool("isGrounded", true);
->>>>>>> Stashed changes
 
         private void Update()
         {
@@ -98,34 +59,8 @@ namespace Gravity
 
         }
 
-<<<<<<< Updated upstream
         void FixedUpdate()
         {
-=======
-        // Apply movement to rigidbody
-        Vector3 localMove = transform.TransformDirection(moveAmount) * Time.fixedDeltaTime;
-        rb.MovePosition(rb.position + localMove);
-    }
-    private void Move()
-    {
-        //References Input Manager and rotates player based on where mouse or right joystick dictates
-        transform.Rotate(Vector3.up * inputManager.cameraInput.x * mouseSensitivityX);
-        verticalLookRotation += inputManager.cameraInput.y * mouseSensitivityY;
-        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -45, -25);
-        cameraTransform.localEulerAngles = Vector3.left * verticalLookRotation;
-
-
-
-        //references InputManager and moves player based on Input System
-        Vector3 moveDirection = new Vector3(inputManager.horizontalInput, 0, inputManager.verticalInput).normalized;
-        
-        Vector3 targetMoveAmount = moveDirection * walkSpeed;
-        moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, .15f);
-        
-    }
-    private void Jump()
-    {
->>>>>>> Stashed changes
 
 
             // Apply movement to rigidbody
@@ -147,12 +82,6 @@ namespace Gravity
         }
         private void Jump()
         {
-<<<<<<< Updated upstream
-=======
-            this.animator.SetTrigger("Jump");
-            rb.AddForce(transform.up * jumpForce);
-            inputManager.jump_Input = false;
->>>>>>> Stashed changes
 
             if (isGrounded)
             {
