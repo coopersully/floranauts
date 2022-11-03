beaa21c2bfdfe198f5754e8eae31c0800c0d0951
Cooper Sullivan <thecoopersully@gmail.com>
2022-11-03 18:46:44 -0500

    Add mound claiming & score
    - Add MoundManager.cs script for claiming mounds & mound types
    - Add PlayerScoreManager.cs for player scores

---
+++ Assets/Scripts/MoundsManager.cs
@@ -0,0 +1,147 @@
+using UnityEngine;
+
+/*Written by Thomas Walker
+ 11/1/2022
+ 
+ Things to Work On
+ - What planet abilities will there be?
+ - What do the planet abilities do?
+ - Does there need to be a copy of everything for each player? How are we doing that?
+ */
+public class MoundsManager : MonoBehaviour
+{
+    //Dropdown of planet abilities
+    public MoundType planetAbility = new();
+    public enum MoundType
+    {
+        None,
+        Jetpack, 
+        DoubleJump, 
+        IncreaseSpeed,
+        Grenade
+    };
+
+    [Header("-Players-")]
+    public GameObject bluePlayer;
+    //public GameObject redPlayer;
+    
+    // [Header("-Player Animations-")]
+    // public Animation plantingTree;
+    // public Animation destroyingTree;
+    
+    [Header("-Planet Variables-")]
+    public GameObject planetTree;
+    public GameObject planetTreeLeaves;
+    public GameObject planet;
+    public Material grayPlanet;
+    public Material bluePlanet;
+    public Material redPlanet;
+
+    // [Header("-Tree Animations-")]
+    // public Animation treeGrowing;
+    // public Animation treeDying;
+    
+    //Boolean Checks
+    private bool _playerIsPlanting;
+    private bool _blueCaptured = false;
+    private bool _redCaptured = false;
+    private bool _blueHasAbility = false;
+    private bool _redHasAbility = false;
+
+    private void Start()
+    {
+        planet.GetComponent<MeshRenderer> ().material = grayPlanet;
+        planetTreeLeaves.GetComponent<MeshRenderer> ().material = grayPlanet;
+        planetTree.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Blue Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                //If the planet is unclaimed
+                if (_blueCaptured == false && _redCaptured == false)
+                {
+                    Debug.Log("Blue Player Claimed Planet");
+                    _blueCaptured = true;
+                    
+                    //play plantingTree animation
+                    //play treeGrowing animation
+                
+                    planetTree.SetActive(true);
+
+                    PlayerScoreManager.bluePlayerPlanetsCount++;
+                
+                    planet.GetComponent<MeshRenderer> ().material = bluePlanet;
+                    planetTreeLeaves.GetComponent<MeshRenderer> ().material = bluePlanet;
+                
+                    //Not sure what will happen exactly when a player gets these abilities...
+                    _blueHasAbility = true;
+                }
+                
+                //if the planet is claimed by the other player
+                if (_redCaptured == true)
+                {
+                    Debug.Log("Blue Player Neutralized Planet");
+                    _redCaptured = false;
+                    
+                    //play destroyingTree animation
+                    //play treeDying animation
+                
+                    planetTree.SetActive(false);
+                    
+                    planet.GetComponent<MeshRenderer> ().material = grayPlanet;
+                    planetTreeLeaves.GetComponent<MeshRenderer> ().material = grayPlanet;
+
+                    _redHasAbility = false;
+                }
+            }
+        }
+        
+        if (other.gameObject.CompareTag("Red Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                //If the planet is unclaimed
+                if (_redCaptured == false && _blueCaptured == false)
+                {
+                    Debug.Log("Red Player Claimed Planet");
+                    _redCaptured = true;
+                    
+                    //play plantingTree animation
+                    //play treeGrowing animation
+                
+                    planetTree.SetActive(true);
+
+                    PlayerScoreManager.redPlayerPlanetsCount++;
+                
+                    planet.GetComponent<MeshRenderer> ().material = redPlanet;
+                    planetTreeLeaves.GetComponent<MeshRenderer> ().material = redPlanet;
+                
+                    //Not sure what will happen exactly when a player gets these abilities...
+                    _redHasAbility = true;
+                }
+                
+                //if the planet is claimed by the other player
+                if (_blueCaptured == true)
+                {
+                    Debug.Log("Blue Player Neutralized Planet");
+                    _blueCaptured = false;
+                    
+                    //play destroyingTree animation
+                    //play treeDying animation
+                
+                    planetTree.SetActive(false);
+                    
+                    planet.GetComponent<MeshRenderer> ().material = grayPlanet;
+                    planetTreeLeaves.GetComponent<MeshRenderer> ().material = grayPlanet;
+
+                    _blueHasAbility = false;
+                }
+            }
+        }
+  
+    }
+}
---
+++ Assets/Scripts/PlayerScoreManager.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+/*Written by Thomas Walker
+ 11/1/2022
+ 
+ Things to Work On
+ - Really hard to trigger the planet capture in OnTriggerEnter
+ - Error trying to access the text in the Score UI
+ 
+ - Does there need to be a copy of everything for each player? How are we doing that?
+ - What happens when you win?
+ */
+
+public class PlayerScoreManager : MonoBehaviour
+{
+    [Header("-Planets Captured by Players-")]
+    public static int bluePlayerPlanetsCount;
+    public static int redPlayerPlanetsCount;
+
+    //Player Scores
+    private int _bluePlayerScore;
+    private int _redPlayerScore;
+
+    [Header("-Score UI-")]
+    public TextMeshProUGUI bluePlayerScoreUI;
+    public TextMeshProUGUI redPlayerScoreUI;
+
+    private void Awake()
+    {
+        _bluePlayerScore = 0;
+        _redPlayerScore = 0;
+        bluePlayerPlanetsCount = 0;
+        redPlayerPlanetsCount = 0;
+        
+        //Sets score UI and begins tracking score
+        StartCoroutine(IterateScore());
+        UpdateScoreUI();
+    }
+
+    private void Update()
+    {
+        
+        UpdateScoreUI();
+        
+        //Keeps the blue player's score from going below zero and activates a win if they reach 100 points
+        if (_bluePlayerScore < 0)
+        {
+            _bluePlayerScore = 0;
+        } else if (_bluePlayerScore >= 100)
+        {
+            BlueWin();
+        }
+        
+        //Keeps the red player's score from going below zero and activates a win if they reach 100 points
+        if (_redPlayerScore < 0)
+        {
+            _redPlayerScore = 0;
+        } else if (_redPlayerScore >= 100)
+        {
+            RedWin();
+        }
+    }
+
+    //Updates the score UI
+    private void UpdateScoreUI()
+    {
+        bluePlayerScoreUI.SetText("Score: " + bluePlayerPlanetsCount);
+        redPlayerScoreUI.SetText("Score: " + redPlayerPlanetsCount);
+    }
+    
+    //Adds total planets captured to each players' score
+    private IEnumerator IterateScore()
+    {
+        while (true)
+        {
+            _bluePlayerScore += bluePlayerPlanetsCount;
+            _redPlayerScore += redPlayerPlanetsCount;
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    private void BlueWin()
+    {
+        Debug.Log("Blue Player wins");
+        StopCoroutine(IterateScore());
+    }
+    
+    private void RedWin()
+    {
+        Debug.Log("Red Player wins");
+        StopCoroutine(IterateScore());
+    }
+}
