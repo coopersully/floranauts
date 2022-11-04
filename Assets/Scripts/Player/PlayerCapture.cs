4f2b5ab9aeffa7d6b9f7b2932e3472b90e1f8cb6
Cooper Sullivan <thecoopersully@gmail.com>
2022-11-03 23:03:39 -0500

    Add notes to capture mechanic

--- Assets/Scripts/Player/PlayerCapture.cs
+++ Assets/Scripts/Player/PlayerCapture.cs
@@ -10,10 +10,15 @@ namespace Player
         private bool _canCapture;
         private PlayerInput _playerInput;
 
+        public int score;
+
         public List<CapturePoint> inventory;
         public CapturePoint currentCapturePoint;
+        
         public Color playerColor;
-
+        public SkinnedMeshRenderer playerSkin;
+        public SkinnedMeshRenderer playerAccents;
+        
         private void Awake()
         {
             _playerInput = GetComponent<PlayerInput>();
@@ -25,14 +30,26 @@ namespace Player
                 4 => Color.blue,
                 _ => playerColor
             };
+            
+            // Colorize the player model
+            playerSkin.material.color = playerColor;
+            playerAccents.material.color = playerColor;
+            
+            // Start incrementing the player's score every second
+            //StartCoroutine(IncrementScore());
         }
-
+        
+        // Called every time a player presses the 'Capture' binding.
         public void Capture(InputAction.CallbackContext context)
         {
             if (!context.started) return;
             currentCapturePoint.CaptureAction(this);
         }
 
+        /* When a player enters a 'CapturePoint' on a planet, set it
+         to their currently selected CapturePoint in the script so that
+         if they press any buttons (above) it will call the function on
+         that CapturePoint. */
         private void OnTriggerEnter(Collider other)
         {
             // If the entered trigger is not a capture point, ignore it.
@@ -40,13 +57,26 @@ namespace Player
 
             currentCapturePoint = other.GetComponent<CapturePoint>();
         }
-
+        
+        /* When a player exits a 'CapturePoint' on a
+         planet, nullify their current capture point. */
         private void OnTriggerExit(Collider other)
         {
             // If the entered trigger is not a capture point, ignore it.
             if (!other.CompareTag("Planet1")) return;
 
             currentCapturePoint = null;
+        }
+        
+        /* Called once every second. Increments the player's
+         score by the amount of Capture Points they currently own. */
+        private IEnumerator IncrementScore()
+        {
+            while (true)
+            {
+                score += inventory.Count;
+                Debug.Log(name + " has a score of " + score);
+            }
         }
     }
 }
