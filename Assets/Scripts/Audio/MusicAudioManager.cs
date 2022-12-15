62c42c4a075032f645598d79a524520ef098af51
Cooper Sullivan <thecoopersully@gmail.com>
2022-12-14 19:41:40 -0600

    Revert "Add experimental main menu"
    This reverts commit 7f51a5e6678954eae06c7ea24ce5ab4c476373e7.

--- Assets/Scripts/Audio/MusicAudioManager.cs
+++ Assets/Scripts/Audio/MusicAudioManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -6,12 +7,38 @@ namespace Audio
     public class MusicAudioManager : MonoBehaviour
     {
         public AudioSource audioSource;
-        public AudioClip[] songs;
+        public AudioClip original01;
+        public AudioClip original02;
+        public AudioClip original03;
+        public AudioClip original04;
+        public AudioClip original05;
+        public AudioClip original06;
 
         private void Awake()
         {
-            audioSource.clip = songs[Random.Range(0, songs.Length)];
-            audioSource.Play();
+            switch (Random.Range(1, 5))
+            {
+                case 1:
+                    audioSource.clip = original01;
+                    audioSource.Play();
+                    break;
+                case 2:
+                    audioSource.clip = original02;
+                    audioSource.Play();
+                    break;
+                case 3:
+                    audioSource.clip = original03;
+                    audioSource.Play();
+                    break;
+                case 4:
+                    audioSource.clip = original04;
+                    audioSource.Play();
+                    break;
+                case 5:
+                    audioSource.clip = original05;
+                    audioSource.Play();
+                    break;
+            }
         }
     }
 }
