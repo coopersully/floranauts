fe42978114a8d66d9e2894d8096d703e4959fb74
MacraeSmith <99697319+MacraeSmith@users.noreply.github.com>
2023-06-03 18:28:15 -0500

    adjusted new planets

---
+++ Assets/Scripts/Planets/MeshSpawnPlanet.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class MeshSpawnPlanet : MonoBehaviour
+{
+    public Material crystalMat;
+    public Material grassMat;
+    public Material greenGlowyMat;
+    public Material iceMat;
+    public Material lavaMat;
+    public Material rootsMat;
+    public Material swirlsMat;
+    public Material waterMat;
+    private Material setMat;
+    public GameObject planetMesh;
+    
+
+    public enum Planet_Mats
+    {
+        crystal,
+        grass,
+        green,
+        ice,
+        lava,
+        swirl,
+        roots,
+        water
+    }
+    public Planet_Mats planetMat;
+    
+    void Start()
+    {
+        
+        switch (planetMat)
+        {
+            case Planet_Mats.crystal:
+                setMat = crystalMat;
+                break;
+            case Planet_Mats.grass:
+                setMat = grassMat;
+                break;
+            case Planet_Mats.green:
+                setMat = greenGlowyMat;
+                break;
+            case Planet_Mats.ice:
+                setMat = iceMat;
+                break;
+            case Planet_Mats.lava:
+                setMat = lavaMat;
+                break;
+            case Planet_Mats.swirl:
+                setMat = swirlsMat;
+                break;
+            case Planet_Mats.roots:
+                setMat = rootsMat;
+                break;
+            case Planet_Mats.water:
+                setMat = waterMat;
+                break;
+
+        }
+        planetMesh.GetComponent<Renderer>().material = setMat;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
