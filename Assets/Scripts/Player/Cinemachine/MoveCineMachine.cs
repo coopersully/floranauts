d701e219a236e12de1949c5fe1d975ee43a7071a
MacraeSmith <99697319+MacraeSmith@users.noreply.github.com>
2023-06-03 20:34:09 -0500

    adjusted movecinemachine

--- Assets/Scripts/Player/Cinemachine/MoveCineMachine.cs
+++ Assets/Scripts/Player/Cinemachine/MoveCineMachine.cs
@@ -66,7 +66,7 @@ namespace Player
             UpdateGroundedValue();
             ApplyMovement();
 
-            if (!isGrounded) Debug.Log("not Grounded");
+            //if (!isGrounded) Debug.Log("not Grounded");
         }
         private void FixedUpdate()
         {
